38c9c8c677974bd0fd44a0b24e83b36e3cee6395
Emily Charles <emily.w.charles@gmail.com>
2022-02-23 22:47:03 -0500

    add dev db pipeline

--- app.py
+++ app.py
@@ -10,9 +10,14 @@ from flask_cors import CORS
 def create_app():
     logging.getLogger("flask_cors").level = logging.DEBUG
     app = Flask(__name__)
+    development = os.environ.get("FLASK_ENV") != "production"
+    db_user = os.environ.get(f"{'DEV_' if development else ''}DATABASE_USER")
+    db_pw = os.environ.get(f"{'DEV_' if development else ''}DATABASE_PW")
+    db_url = os.environ.get(f"{'DEV_' if development else ''}DATABASE_URL")
+    print(db_url)
     app.config[
         "SQLALCHEMY_DATABASE_URI"
-    ] = f"mysql://{os.environ.get('DATABASE_USER')}:{os.environ.get('DATABASE_PW')}@{os.environ.get('DATABASE_URL')}/{os.environ.get('DATABASE_NAME')}"
+    ] = f"mysql://{db_user}:{db_pw}@{db_url}/{os.environ.get('DATABASE_NAME')}"
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     app.config["CORS_HEADERS"] = "Content-Type"
     app.config["CORS_ORIGINS"] = "*"
